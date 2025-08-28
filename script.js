920a29799a28237594c9f0e753a9aa82c9d7b4fb
mhmasum1 <mhmasum231@gmail.com>
2025-08-28 12:04:37 +0600

    Calling alert shown

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Heart Count 
+
 let heartCount = 0;
 const heartCounts = document.getElementById("heartCount");
 const heartIcons = document.querySelectorAll(".heart-icon");
@@ -8,6 +10,19 @@ for (const findIcon of heartIcons) {
         heartCount = heartCount + 1;
         heartCounts.innerText = heartCount;
 
+
+    });
+}
+
+// Call Buttons
+
+let availableCoins = 100;
+const coins = document.getElementById("available-coins");
+const calls = document.querySelectorAll("#call-button")
+for (const call of calls) {
+    call.addEventListener('click', function (e) {
+        e.stopPropagation();
+        alert("Calling")
 
     });
 }
